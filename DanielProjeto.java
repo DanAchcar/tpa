860f98a74c4883445d1bb0967a41ef0421e0ce3d
DanAchcar <achcardaniel8@gmail.com>
2025-04-09 15:01:23 -0300

    Add files via upload

---
+++ DanielProjeto.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.daniel.projeto;
+
+/**
+ *
+ * @author FATEC ZONA LESTE
+ */
+public class DanielProjeto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");// Declaração de variáveis primitivas
+           byte caneta = 25; // Armazena números pequenos (-128 a 127)
+           short cadeira = 2025; // armazena valores um pouco maióres (-32.767)
+           int estojo = 500000; // Tipo mais comum para números inteiros
+            long oculos = 8000000000L; // Necessário o "L" para valores
+
+        float chinelo = 1.75f; // Necessário o "F" no final para indical float
+        boolean meia = true; // Armazena verdadeiro ou falso
+
+        char dinheiro = 'J'; // Armazena um único caractere
+        boolean entendi = true; // Armazena verdadeiro ou falso
+
+    // Exibição dos valores
+    System.out.println("caneta: " + caneta);
+    System.out.println("cadeira: " + cadeira);
+    System.out.println("estojo: " + estojo);
+    System.out.println("óculos:) " + oculos);
+    System.out.println("chinelo: " + chinelo + "m");
+    System.out.println(":meia " + meia + "Kg");
+    System.out.println("dinheiro: " + dinheiro);
+    System.out.println("entendeu? " + entendi);
+
+}
+
+}
+    
+
